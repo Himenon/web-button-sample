e22c769c85afa943f9dce99165a737b2adcd987e
Himenon <k.himeno314@gmail.com>
2022-06-12 21:36:30 +0900

    feat: add query params

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BlockingButton } from "./BlockingButton";
 import { ThrottleButton } from "./ThrottleButton";
 import { DebounceButton } from "./DebounceButton";
 import { LinearProgressWithLabel } from "./LinearWithValueLabel";
+import { useShowButton } from "./useShowButton";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -15,6 +16,7 @@ const wait = async (ms: number) =>
 
 export const App = () => {
   const [value, updateCount] = useState(0);
+  const { kind } = useShowButton();
 
   return (
     <Grid
@@ -24,40 +26,52 @@ export const App = () => {
       alignItems="center"
       style={{ minHeight: "100vh" }}
     >
-      <Grid item xs={3}>
+      <Grid item xs={3} style={{ minWidth: "256px" }}>
         <Card>
           <CardContent>
             <Box>
               <LinearProgressWithLabel value={value} />
             </Box>
-            <ButtonGroup size="small" variant="outlined" aria-label="outlined button group">
-              <DefaultButton
-                onClick={() => {
-                  updateCount((prev) => prev + 1);
-                }}
-                text="Default"
-              />
-              <BlockingButton
-                text="Block"
-                onClick={async () => {
-                  await wait(1000);
-                  updateCount((prev) => prev + 1);
-                }}
-              />
-              <ThrottleButton
-                text="Throttle"
-                onClick={() => {
-                  updateCount((prev) => prev + 1);
-                }}
-                timeoutMs={1000}
-              />
-              <DebounceButton
-                text="Debounce"
-                onClick={() => {
-                  updateCount((prev) => prev + 1);
-                }}
-                timeoutMs={1000}
-              />
+            <ButtonGroup
+              size="small"
+              variant="outlined"
+              aria-label="outlined button group"
+            >
+              {["all", "default"].includes(kind) && (
+                <DefaultButton
+                  onClick={() => {
+                    updateCount((prev) => prev + 1);
+                  }}
+                  text="Default"
+                />
+              )}
+              {["all", "block"].includes(kind) && (
+                <BlockingButton
+                  text="Block"
+                  onClick={async () => {
+                    await wait(1000);
+                    updateCount((prev) => prev + 1);
+                  }}
+                />
+              )}
+              {["all", "throttle"].includes(kind) && (
+                <ThrottleButton
+                  text="Throttle"
+                  onClick={() => {
+                    updateCount((prev) => prev + 1);
+                  }}
+                  timeoutMs={1000}
+                />
+              )}
+              {["all", "debounce"].includes(kind) && (
+                <DebounceButton
+                  text="Debounce"
+                  onClick={() => {
+                    updateCount((prev) => prev + 1);
+                  }}
+                  timeoutMs={1000}
+                />
+              )}
             </ButtonGroup>
           </CardContent>
         </Card>
---
+++ src/useShowButton.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export type Kind = "all" | "default" | "blocking" | "throttle" | "debounce";
+
+export interface Query {
+  kind: "all" | "default" | "blocking" | "throttle" | "debounce";
+}
+
+const kinds: Kind[] = ["all", "default", "blocking", "throttle", "debounce"];
+
+const isKind = (text: string | undefined): text is Kind => {
+  return kinds.includes(text as unknown as any);
+}
+
+export const useShowButton = (): { kind: Kind } => {
+  const instance = new URLSearchParams(location.search);
+  const queryParams: Record<string, string> = {};
+  for (const pair of instance.entries()) {
+    queryParams[pair[0]] = pair[1];
+  }
+  const [kind] = useState(isKind(queryParams.kind) ? queryParams.kind : "all");
+  return {
+    kind,
+  }
+}
